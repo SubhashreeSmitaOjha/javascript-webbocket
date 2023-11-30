8a58282e33e9d7fbd79400ce91f17e1cb8b5987e
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-11-30 15:02:22 +0000

    file to be loaded

---
+++ 01-Basics/02.datatype.js
@@ -0,0 +1,27 @@
+let num1=568;
+     let num2=568.38;
+     console.log(typeof(num1));
+     console.log(typeof(num2));
+
+     var num=10;
+     var a='5';
+     res=num+a;
+     console.log(res);
+     console.log(typeof(res));
+
+     let name="Subhashree";
+     let age=18;
+     let isloggedIn=true;
+     let myId;
+     const mySalary=34352;
+     let myBonus=null;
+     console.log(typeof(name));
+     console.log(typeof(age));
+     console.log(typeof(isloggedIn));
+     console.log(typeof(myId));
+     console.log(typeof(mySalary));
+     console.log(typeof(myBonus));
+
+     let Id=Symbol("123");
+     let UserId=Symbol("123");
+     console.log(Id==UserId);
