4a4e0270a874be337f677752fecce7bcc99058da
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-01 06:21:44 +0000

    file to be loaded

--- 01-Basics/04-controlStatement.js
+++ 01-Basics/04-controlStatement.js
@@ -8,3 +8,8 @@ it is like an expression which control the expression of a program.
         --here we also give an expression and until the condition met repeatedly continue the iteration.
         --for loop(for in,for of),while(entry control loop),do-while(exit control loop)
 */
+// if-statement
+    let a=10;
+    if (a<=20) {
+        console.log("a is smallest");
+    }
