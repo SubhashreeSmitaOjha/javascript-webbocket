0ce32d6b07810984863dde42cf7be3e7f9b8b689
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-01 05:56:34 +0000

    file to be loaded

---
+++ 01-Basics/03-operator.js
@@ -0,0 +1,16 @@
+/*
+1.assignment(=,+=,-=,*=,%=,/=)
+2.arithmatic(+,-,%,*,/,**)
+3.comparision(>,<,==,<=,>=,/=)
+4.logical(&&,||,!)
+5.bitwise(&,|,~,^,<<,>>,>>>)
+6.typeof(typeof,instanceof)
+7.string(concatination)
+    Ex-let fname="Subhashree";
+        let lname="Smita";
+        let name=fname+lname;
+ */
+        let fname="Subhashree";
+        let lname="Smita";
+        let name=fname+lname;
+        console.log(name);
