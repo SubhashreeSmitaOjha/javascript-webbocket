b9175432696855490312b1c6838d783fb0dc4553
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-01 06:14:12 +0000

    file to be loaded

--- 01-Basics/04-controlStatement.js
+++ 01-Basics/04-controlStatement.js
@@ -6,5 +6,5 @@ it is like an expression which control the expression of a program.
         --if,if-else,else-if,switch,ternary
     2.iterative
         --here we also give an expression and until the condition met repeatedly continue the iteration.
-        --for loop,while,do-while
+        --for loop(for in,for of),while(entry control loop),do-while(exit control loop)
 */
