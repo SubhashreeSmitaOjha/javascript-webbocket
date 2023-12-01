44def2fef4d8545df3c6928ebba6aba2d6dc69ab
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-01 06:02:45 +0000

    file to be loaded

--- 01-Basics/03-operator.js
+++ 01-Basics/03-operator.js
@@ -14,3 +14,15 @@
         let lname="Smita";
         let name=fname+lname;
         console.log(name);
+
+/*
+==(used to check same or not(return true)(check only value))
+===(used to check value with datatype)
+*/
+    let a=5;
+    let b='5';
+    console.log(a==b);
+
+    let a1=5;
+    let b1="5";
+    console.log(a1===b1);
