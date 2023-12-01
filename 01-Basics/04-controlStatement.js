27c4680fa8b5164493532203971a229424165553
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-01 06:12:46 +0000

    file to be loaded

---
+++ 01-Basics/04-controlStatement.js
@@ -0,0 +1,10 @@
+/*
+it is like an expression which control the expression of a program.
+2types.
+    1.conditional
+        --here we provide condition and based on that condition rest part of the program are executed.
+        --if,if-else,else-if,switch,ternary
+    2.iterative
+        --here we also give an expression and until the condition met repeatedly continue the iteration.
+        --for loop,while,do-while
+*/
