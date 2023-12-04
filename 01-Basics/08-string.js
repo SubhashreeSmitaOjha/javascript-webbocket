d7dd0b0c100811434cd93e2035ccff00b15f44ba
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-04 10:40:39 +0000

    file to be loaded

--- 01-Basics/08-string.js
+++ 01-Basics/08-string.js
@@ -4,4 +4,5 @@
     let lname=" Ojha";
     let age=24;
     console.log("My name is"+name+" "+lname+" ,I am "+age+"years old.");
+    // template literals(used by``)
     console.log(`My name is ${name} ${lname}, I am ${age} years old.`);
