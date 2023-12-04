f1783f72ea252992234881c7974dff8b863ad61d
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-04 10:37:46 +0000

    file to be loaded

---
+++ 01-Basics/08-string.js
@@ -0,0 +1,7 @@
+// String
+// Ex-1
+    let name="Subhashree Smita";
+    let lname=" Ojha";
+    let age=24;
+    console.log("My name is"+name+" "+lname+" I am "+age+"years old");
+    console.log(`My name is ${name} ${lname} I am ${age} years old`);
