6d1586e5746de32d47cc1a6f33687e104a8ee6e3
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-04 10:54:11 +0000

    file to be loaded

--- 01-Basics/08-string.js
+++ 01-Basics/08-string.js
@@ -13,3 +13,10 @@
     console.log(name1.toLowerCase());
     console.log(name1.indexOf('b'));
     console.log(name1.charAt(7));
+// Ex-3
+    const newName=name1.SubString(0,4);
+    console.log(newName);
+// Ex-4
+    const myName="____gift____";
+    console.log(myName);
+    console.log(myName.trim());
