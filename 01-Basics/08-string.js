fed9c16fca8a0f68d4e506a7508772a106be8f3b
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-04 10:47:18 +0000

    file to be loaded

--- 01-Basics/08-string.js
+++ 01-Basics/08-string.js
@@ -6,3 +6,10 @@
     console.log("My name is "+name+" "+lname+" ,I am "+age+"years old.");
     // template literals(used by``)
     console.log(`My name is ${name} ${lname}, I am ${age} years old.`);
+// Ex-2
+    const name1=new String("Bhubaneswar");
+    console.log(name1.length);
+    console.log(name1.toUpperCase());
+    console.log(name1.toLowerCase());
+    console.log(name1.indexOf('b'));
+    console.log(name1.charAt(7));
