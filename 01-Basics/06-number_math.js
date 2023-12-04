16cd737c4c12b89c8c9cf9a66ca2191a3435aacc
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-04 09:57:47 +0000

    file to be loaded

---
+++ 01-Basics/06-number_math.js
@@ -0,0 +1,16 @@
+// Ex-1
+    const num=768;
+    console.log(num);
+// Ex-2
+    // new(datatype),Number(reserved keyword)
+    const newScore = new Number(100);
+    console.log(newScore);
+    console.log(typeof newScore);
+    console.log(newScore.toString());
+    console.log(newScore.toString().length);
+    console.log(newScore.toFixed(2));
+// Ex-3
+    const otherNumber=125.8920;
+    console.log(otherNumber.toPrecision(4));
+    const demoNumber=1000000;
+    console.log(demoNumber.toLocaleString());
