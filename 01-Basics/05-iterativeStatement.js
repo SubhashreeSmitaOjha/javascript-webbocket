68ac1c9e43f90bba3697648c0ad37499c7be9406
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-04 09:15:29 +0000

    file to be loaded

--- 01-Basics/05-iterativeStatement.js
+++ 01-Basics/05-iterativeStatement.js
@@ -12,3 +12,10 @@
         console.log(b);
         b++;
     } while (b<50);
+
+// for in(return index),for of(index,value),for each(object,function)
+// Ex-3
+    for (let x = 0; x <= 10; x++) {
+        console.log(x);
+        
+    }
