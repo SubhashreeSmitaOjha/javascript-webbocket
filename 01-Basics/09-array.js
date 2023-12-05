361c438796bf1750a89bdd1a81924fed3e1eb054
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-05 09:01:00 +0000

    file to be loaded

---
+++ 01-Basics/09-array.js
@@ -0,0 +1,16 @@
+/*array ['num','float','string','boolean',null]
+1.an array is a special variable which can hold more than one variable.
+2.in js wecan store different types of data in array.
+*/
+// we can defined:-
+    // Ex-1
+        const myArr=[1,2,3,'name1','name2'];
+        console.log(myArr);
+    // Ex-2
+        const getArr=new Array(1,2,3,4,5);
+        console.log(getArr);
+    // Ex-3
+        // const getArr1[0] ="ABC";
+        // const getArr1[1]="XYZ";
+        // const getArr1[2]="PQR";
+        // console.log(getArr1);
