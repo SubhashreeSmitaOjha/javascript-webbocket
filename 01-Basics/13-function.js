67975c7cde251209a4dff79bddf3b5ba004626c3
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-07 10:09:45 +0000

    file to be loaded

---
+++ 01-Basics/13-function.js
@@ -0,0 +1,16 @@
+function name() {
+    console.log("S");
+    console.log("u");
+    console.log("b");
+    console.log("h");
+    console.log("a");
+    console.log("S");
+    console.log("h");
+    console.log("r");
+    console.log("e");
+    console.log("e");
+}
+name();
+name();
+name();
+name();
