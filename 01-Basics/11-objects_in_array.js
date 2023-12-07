e2122dada9cd8077cd2ce909c5f9a7551b0ac734
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-07 09:46:33 +0000

    file to be loaded

---
+++ 01-Basics/11-objects_in_array.js
@@ -0,0 +1,22 @@
+// Ex-1
+    const arrObj=[
+    {
+        id: 121,
+        location: "bbsr"
+    },
+    {
+        id: 122,
+        location: "cuttack"
+    },
+    {
+        id: 123,
+        location: "puri"
+    },
+    {
+        id: 124,
+        location: "jagatsingpur"
+    }
+]
+console.log(arrObj);
+console.log(arrObj[3]);
+console.log(arrObj[2]);
