ce15cb3a9ce5ed8ce9241c400ed345871710bcca
SubhashreeSmitaOjha <152278051+SubhashreeSmitaOjha@users.noreply.github.com>
2023-12-07 09:05:52 +0000

    file to be loaded

---
+++ 01-Basics/10-object_datastructure.js
@@ -0,0 +1,16 @@
+// Ex-1(object literals)
+    const person={
+        name:"Web_Bocket",
+        age:24,
+        location:"BBSR"
+    };
+    console.log(person);
+    console.log(person.location);
+// Ex-2(object constructor)
+    const person1={};
+        person1.name="Web_Bocket";
+        person1.age=24;
+        person1.location="BBSR";
+    console.log(person1);
+    console.log(person1.name);
+    console.log(person1.location);
